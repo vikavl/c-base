37a97a6505e06799666a0758134cf06e1c850678
vikavl <48601991+vikavl@users.noreply.github.com>
2019-03-21 23:07:10 +0200

    Add files via upload
    My first hello-world program for c++ base (itea)!

---
+++ first.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+	printf("Hello world! My name is Vika and I love programming!\n");
+	std::cout << "And also c++ programm language!" << std::endl;
+	system("pause");
+	return 0;
+}
