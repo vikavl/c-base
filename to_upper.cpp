48abfab61da8bbbc9445702357631a5a143097b2
vikavl <48601991+vikavl@users.noreply.github.com>
2019-04-09 01:48:11 +0300

    Update to_upper.cpp

--- to_upper.cpp
+++ to_upper.cpp
@@ -1,21 +1,25 @@
-//#include <iostream>
-//#include <conio.h>
-//
-//int main() {
-////==================================================TO UPPER==========================================================//
-//	const int SIZE = 255;
-//	char ch;
-//	do {
-//	char str[SIZE];
-//	std::cout << "Enter string: ";	std::cin >> str;
-//	for (size_t i = 0; i < strlen(str); i++) {
-//		if ((str[i]>=97)&&(str[i]<=122)) {
-//			str[i] = str[i] - 32;
-//			//std::cout << str[i] << std::endl;
-//		}
-//		std::cout << str[i] ;
-//	}
-//		std::cout << "\nTry again? [ENTER/n]";
-//		ch = _getch();
-//	} while ((ch != 'n') && (ch != 'N'));
-//}
+#include <iostream>
+#include <conio.h>
+
+const int SIZE = 255;
+
+void to_upper(char s[SIZE]) {
+	for (size_t i = 0; i < strlen(s); i++) {
+		if ((s[i] >= 97) && (s[i] <= 122)) {
+			s[i] = s[i] - 32;
+		}
+		std::cout << s[i];
+	}
+}
+
+int main() {
+//==================================================TO UPPER==========================================================//
+	char ch;
+	do {
+	char str[SIZE];
+	std::cout << "Enter string: ";	std::cin >> str;
+	to_upper(str);
+		std::cout << "\nTry again? [ENTER/n]\n";
+		ch = _getch();
+	} while ((ch != 'n') && (ch != 'N'));
+}
