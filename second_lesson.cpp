1adb04998173a90ef2ea0e0974c1382b2e6f46f6
vikavl <48601991+vikavl@users.noreply.github.com>
2019-04-09 02:04:32 +0300

    Update second_lesson.cpp

--- second_lesson.cpp
+++ second_lesson.cpp
@@ -4,12 +4,14 @@
 
 int main() {
 	float a, b, c, D, x1, x2, x;
+	const int TWO=2,
+		FOUR=4;
 
 	std::cout << "Enter a="; std::cin >> a;
 	std::cout << "Enter b="; std::cin >> b;
 	std::cout << "Enter c="; std::cin >> c;
 
-	D = pow(b, 2) - (4 * a*c);
+	D = pow(b, TWO) - (FOUR * a*c);
 
 	if (D >= 0) {
 		if (a == 0) {
@@ -17,8 +19,8 @@ int main() {
 			std::cout << "x=" << x << std::endl;
 		}
 		else {
-			x1 = (-b + sqrt(D)) / (2 * a);
-			x2 = (-b - sqrt(D)) / (2 * a);
+			x1 = (-b + sqrt(D)) / (TWO * a);
+			x2 = (-b - sqrt(D)) / (TWO * a);
 			std::cout << "x1=" << x1 << std::endl;
 			std::cout << "x2=" << x2 << std::endl;
 		}
