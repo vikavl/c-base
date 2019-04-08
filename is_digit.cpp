a3c95a3409e3b514a819dc5df8b5405857c34fc2
vikavl <48601991+vikavl@users.noreply.github.com>
2019-04-08 23:36:42 +0300

    Add files via upload
    Is_digit()  for c++ base (itea).

---
+++ is_digit.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//#include <conio.h>
+//#include <string.h>
+//
+//int main() {
+////======================================================IS DIGIT======================================================//
+//	const int SIZE = 255;
+//	char str[SIZE];
+//	std::cout << "Enter string: ";	std::cin >> str;
+//	for (size_t i = 0; i < strlen(str); i++) {
+//		if ((str[i]>=48)&&(str[i]<=57)) {
+//			std::cout << str[i] << std::endl;
+//		}
+//	}
+//	_getch();
+//}
