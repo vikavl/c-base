3c16276bdd5c1e07100ff8777b0c5c07c5c59bf2
vikavl <48601991+vikavl@users.noreply.github.com>
2019-04-09 00:30:06 +0300

    Update build number.cpp

--- build number.cpp
+++ build number.cpp
@@ -1,32 +1,32 @@
-//#include <iostream>
-//#include <conio.h>
-//
-//int main() {
-////=====================================================BUILD NUMBER=============================================================//
-//	char ch;
-//	puts("Divisibility by 3");
-//	do{
-//		int n;
-//		std::cout << "\nEnter amount of numbers "; 
-//		do {
-//			std::cin >> n;
-//		} while (n < 0);
-//		int a;
-//		int sum = 0;
-//		for (int i = 0; i < n; i++) {
-//			std::cout << "Enter number "; 
-//			do {
-//				std::cin >> a;
-//			} while (a < 0);
-//			sum += a;
-//		}
-//		if ((sum % 3) == 0) {
-//			std::cout << "YES" << std::endl;
-//		}
-//		else{
-//			std::cout << "NO" << std::endl;
-//		}
-//		std::cout << "Try again? [ENTER/n]";
-//		ch = _getch();
-//	} while ((ch != 'n') && (ch != 'N'));
-//}
+#include <iostream>
+#include <conio.h>
+
+int main() {
+//=====================================================BUILD NUMBER=============================================================//
+	char ch;
+	puts("Divisibility by 3");
+	do{
+		int n;
+		std::cout << "\nEnter amount of numbers "; 
+		do {
+			std::cin >> n;
+		} while (n < 0);
+		int a;
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			std::cout << "Enter number "; 
+			do {
+				std::cin >> a;
+			} while (a < 0);
+			sum += a;
+		}
+		if ((sum % 3) == 0) {
+			std::cout << "YES" << std::endl;
+		}
+		else{
+			std::cout << "NO" << std::endl;
+		}
+		std::cout << "Try again? [ENTER/n]";
+		ch = _getch();
+	} while ((ch != 'n') && (ch != 'N'));
+}
