8892f515c832566f3e63fdb7f9808388fab33b0d
vikavl <48601991+vikavl@users.noreply.github.com>
2019-04-08 23:37:13 +0300

    Add files via upload
    To_upper()  for c++ base (itea).

---
+++ to_upper.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <conio.h>
+//
+//int main() {
+////==================================================TO UPPER==========================================================//
+//	const int SIZE = 255;
+//	char ch;
+//	do {
+//	char str[SIZE];
+//	std::cout << "Enter string: ";	std::cin >> str;
+//	for (size_t i = 0; i < strlen(str); i++) {
+//		if ((str[i]>=97)&&(str[i]<=122)) {
+//			str[i] = str[i] - 32;
+//			//std::cout << str[i] << std::endl;
+//		}
+//		std::cout << str[i] ;
+//	}
+//		std::cout << "\nTry again? [ENTER/n]";
+//		ch = _getch();
+//	} while ((ch != 'n') && (ch != 'N'));
+//}
