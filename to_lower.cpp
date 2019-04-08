81fd121b830543e1e00b12710a5829c022430f21
vikavl <48601991+vikavl@users.noreply.github.com>
2019-04-09 01:52:57 +0300

    Update to_lower.cpp

--- to_lower.cpp
+++ to_lower.cpp
@@ -1,21 +1,25 @@
-//#include <iostream>
-//#include <conio.h>
-//
-//int main() {
-//	//==================================================TO LOWER==========================================================//
-//	const int SIZE = 255;
-//	char ch;
-//	do {
-//		char str[SIZE];
-//		std::cout << "Enter string: ";	std::cin >> str;
-//		for (size_t i = 0; i < strlen(str); i++) {
-//			if ((str[i] >= 65) && (str[i] <= 90)) {
-//				str[i] = str[i] + 32;
-//				//std::cout << str[i] << std::endl;
-//			}
-//			std::cout << str[i];
-//		}
-//		std::cout << "\nTry again? [ENTER/n]";
-//		ch = _getch();
-//	} while ((ch != 'n') && (ch != 'N'));
-//}
+#include <iostream>
+#include <conio.h>
+
+const int SIZE = 255;
+
+void to_lower(char s[SIZE]) {
+	for (size_t i = 0; i < strlen(s); i++) {
+			if ((s[i] >= 65) && (s[i] <= 90)) {
+				s[i] = s[i] + 32;
+			}
+			std::cout << s[i];
+		}
+}
+
+int main() {
+	//==================================================TO LOWER==========================================================//
+	char ch;
+	do {
+		char str[SIZE];
+		std::cout << "Enter string: ";	std::cin >> str;
+		to_lower(str);
+		std::cout << "\nTry again? [ENTER/n]";
+		ch = _getch();
+	} while ((ch != 'n') && (ch != 'N'));
+}
